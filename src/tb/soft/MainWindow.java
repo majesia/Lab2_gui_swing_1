932521adefeb7fd949c05bf626ee413cb52e1924
majesia <Wiktoria.majewska02@gmail.com>
2022-12-05 18:06:05 +0100

    stworzenie klasy

--- src/tb/soft/MainWindow.java
+++ src/tb/soft/MainWindow.java
@@ -1,11 +1,14 @@
 package tb.soft;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * klasa główna zawierająca metodę statyczną main
  */
 public class MainWindow {
     public static void main(String[] args) {
-        System.out.println("Program konsolowy");
-        System.exit(0);
+
     }
 }
---
+++ src/tb/soft/Window.java
@@ -0,0 +1,19 @@
+package tb.soft;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame implements ActionListener {
+
+
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
