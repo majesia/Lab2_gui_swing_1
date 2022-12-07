8dfab2acf9589ce169069046de0affc71772c215
majesia <Wiktoria.majewska02@gmail.com>
2022-12-07 08:59:21 +0100

    dodanie przycisków i stworzenie okienka w main

--- src/tb/soft/MainWindow.java
+++ src/tb/soft/MainWindow.java
@@ -1,5 +1,6 @@
 package tb.soft;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,8 +8,31 @@ import java.util.List;
 /**
  * klasa główna zawierająca metodę statyczną main
  */
+
+/*
+jframe
+
+guziki ok oraz clear (Login/cancel)
+okienko logowania (textfieldy login, haslo (zahaslowane JPasswordField))
+funkcja main odpowiednio mała, invokeLater wywolywany
+
+
+login powinien sprawdzic w "bazie danych" (kolekcji, mapa spk)
+jesli login i haslo poprawne to tlo okienka zmienia sie na zielone
+jesli nie zgadzaja to sie robi czerwone i np dostajemy druga probe. Po 3 probach cala aplikacja sie zamyka.
+clear - reset i kolejna proba, zamkniecie aplikacji krzyzykiem
+
+kontrolka hasla nie mma operacji gettext i nie wolno pobierac z tego textu dlatego ze string pamieta sie w pamieci nawet op zamknieciu aplikacji
+getpassword zamiast tego
+
+porownujemy hasla znak po znaku
+*/
+
 public class MainWindow {
     public static void main(String[] args) {
 
+        Window okno= new Window();
+        okno.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        okno.setVisible(true);
     }
 }
--- src/tb/soft/Window.java
+++ src/tb/soft/Window.java
@@ -6,6 +6,21 @@ import java.awt.event.ActionListener;
 
 public class Window extends JFrame implements ActionListener {
 
+    JButton bLogin, bCancel;
+    JTextField tfLogin, tfPassword;
+    public Window(){
+        setSize(500,250);
+        setTitle("Logowanie");
+        setLayout(null);
+
+        bLogin = new JButton("Accept");
+        bLogin.setBounds(100,150,100,40);
+        add(bLogin);
+        bCancel = new JButton("Cancel");
+        bCancel.setBounds(300,150,100,40);
+        add(bCancel);
+
+    }
 
 
 
